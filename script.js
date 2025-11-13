4b4af567b7d1ce757bc7039acee31504dc45d2bb
Syed Samad <syedsamad313@gmail.com>
2025-11-13 23:58:35 +0530

    Day 04 - FSModule

--- script.js
+++ script.js
@@ -14,13 +14,36 @@ const fs = require('fs');
 //     else console.log('done with append!');
 // })
 
-// 3. copyFile
+//! 3. copyFile - to copy a file
+//* fs.copyFile(filePath, newCopyFilePath, callback fn)
+// fs.copyFile('fsModule/01-File.txt', 'fsModule/01-FileCopy2.txt', (err) => {
+//     if(err) console.log(err);
+//     else console.log('done with copyfile');
+// })
 
 //! 4. rename - to rename the file
 //* fs.rename(oldName, newName, callback fn)
-fs.rename('fsModule/01File.txt', 'fsModule/01-File.txt', (err) => {
-    if(err) console.log(err);
-    else console.log('done with rename');
-})
+// fs.rename('fsModule/01File.txt', 'fsModule/01-File.txt', (err) => {
+//     if(err) console.log(err);
+//     else console.log('done with rename');
+// })
 
-// 5. unlink
+//! 5. unlink - to delete file
+//* fs.unlink(filePath, callback fn)
+// fs.unlink('fsModule/01-FileCopy2.txt', (err) => {
+//     if(err) console.log(err.message);
+//     else console.log('done with unlink');
+// })
+
+//! 5. rmdir / rm - used to remove directory/folder
+//* fs.rm(folderPath[, options], callback fn)
+//todo: without options we can only delete empty dir
+// fs.rmdir('./copy/', (err) => {
+//     if(err) console.log(err.message);
+//     else console.log('done with removing directory');
+// })
+//todo: add option object with { recursive: true } to delete file loaded directory!
+// fs.rm('./copy/', { recursive: true }, (err) => {
+//     if(err) console.log(err.message);
+//     else console.log('done with removing directory');
+// })
